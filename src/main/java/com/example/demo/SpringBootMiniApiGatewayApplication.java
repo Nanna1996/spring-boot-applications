2d1b637854bea82257edd9d15dc9ebb6298ff41b
Hkathi <Harsha@gmail.com>
2022-11-24 11:38:56 +0530

    CommitMessage

---
+++ src/main/java/com/example/demo/SpringBootMiniApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMiniApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMiniApiGatewayApplication.class, args);
+	}
+
+}
